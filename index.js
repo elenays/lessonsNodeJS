3a830a02d98303cbb003b60dd7c47a8820e9c1ba
elenays <helenasp@yandex.ru>
2020-05-20 11:50:02 +0300

    add hbs and style

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const express = require('express')
-const path = require('path')
+const exphbs = require('express-handlebars')
 
 const app = express()
 
+// ────────────────────────────────────────────────────────────────────────────────
+const hbs = exphbs.create({
+    defaultLayout: 'main', //название дефолтного файла
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine) //регистрируем в JS что есть такой движок
+app.set('view engine', 'hbs') //используем
+app.set('views', 'views') //где храним шаблоны
+// ────────────────────────────────────────────────────────────────────────────────
+
 app.get('/', (rec, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+    res.render('index')
 })
 
 app.get('/about', (rec, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'about.html'))
+    res.render('about')
 })
 
-
+// ────────────────────────────────────────────────────────────────────────────────
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${ PORT }`)
 })
+
