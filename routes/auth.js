4805ce61186b7901d5f6fe09c03014a410110542
elenays <helenasp@yandex.ru>
2020-07-07 17:15:11 +0300

    валидация регистрации

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const { Router } = require('express')
 const User = require('../models/user')
 const bcrypt = require('bcryptjs')
+const { validationResult } = require('express-validator/check')
+const { registerValidators } = require('../utils/validators')
 
 const router = Router()
 
@@ -50,23 +52,22 @@ router.post('/login', async (req, res) => {
     }
 })
 
-router.post('/register', async (req, res) => {
+router.post('/register', registerValidators, async (req, res) => {
     try {
-        const { email, password, repeat, name } = req.body
+        const { email, password, confirm, name } = req.body
         const candidate = await User.findOne({ email })
 
-        if (candidate) {
-            req.flash('registerError', 'Пользователь с таким email уже существует')
-            res.redirect('/auth/login#register')
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            req.flash('registErrror', errors.array()[0].msg)
+            return res.status(422).redirect('/auth/login#register')
         }
-        else {
-            const hashPassword = await bcrypt.hash(password, 10)
-            const user = new User({
-                email, name, password: hashPassword, cart: { items: [] }
-            })
-            await user.save()
-            res.redirect('/auth/login#login')
-        }
+        const hashPassword = await bcrypt.hash(password, 10)
+        const user = new User({
+            email, name, password: hashPassword, cart: { items: [] }
+        })
+        await user.save()
+        res.redirect('/auth/login#login')
     } catch (err) {
         console.log(err)
     }
---
+++ utils/validators.js
@@ -0,0 +1,35 @@
+const { body } = require('express-validator/check')
+const User = require('../models/user')
+
+exports.registerValidators = [
+    body('email', 'Введите корректный email')
+        .isEmail()
+        .custom(async (value, { req }) => {
+            try {
+                const user = await User.findOne({ email: value })
+                if (user) return Promise.reject('Такой email уже занят')
+
+            } catch (err) {
+                console.log(err)
+            }
+        })
+        .normalizeEmail()
+    ,
+    body('password', 'Введите пароль')
+        .isLength({ min: 6, max: 56 })
+        .isAlphanumeric()
+        .trim()
+    ,
+    body('confirm')
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Пароли должны совпадать')
+            }
+            return true
+        })
+        .trim()
+    ,
+    body('name', 'Имя должно быть минимум 3 символа')
+        .isLength({ min: 3 })
+        .trim()
+]
